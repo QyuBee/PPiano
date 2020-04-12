b003c87b8ad6e9e1684b2f1eb3252d1fbfce393e
Quentin <quentinbaron.qb@gmail.com>
2020-04-12 21:28:29 +0200

    Code avec la structure de record enumeration

--- prog.cpp
+++ prog.cpp
@@ -1,199 +1,57 @@
 /*==============================================================================
-Custom DSP Example
+Record enumeration example
 Copyright (c), Firelight Technologies Pty, Ltd 2004-2020.
 
-This example shows how to add a user created DSP callback to process audio
-data. The read callback is executed at runtime, and can be added anywhere in
-the DSP network.
+This example shows how to enumerate the available recording drivers on this
+device. It demonstrates how the enumerated list changes as microphones are
+attached and detached. It also shows that you can record from multi mics at
+the same time.
+
+Please note to minimize latency care should be taken to control the number
+of samples between the record position and the play position. Check the record
+example for details on this process.
 ==============================================================================*/
-#include <fmod.h>
-#include <fmod.hpp>
-#include <fmod_errors.h>
+#include "fmod.hpp"
 #include "common.h"
+#include <vector>
 #include <iostream>
 
-#include <vector>
 
-typedef struct
+static const int MAX_DRIVERS = 2;
+static const int MAX_DRIVERS_IN_VIEW = 3;
+
+struct RECORD_STATE
 {
-    float *buffer;
-    float volume_linear;
-    int   length_samples;
-    int   channels;
-} mydsp_data_t;
+    FMOD::Sound *sound;
+    FMOD::Channel *channel;
+};
 
-FMOD_RESULT F_CALLBACK myDSPCallback(FMOD_DSP_STATE *dsp_state, float *inbuffer, float *outbuffer, unsigned int length, int inchannels, int *outchannels)
+FMOD_RESULT F_CALLBACK SystemCallback(FMOD_SYSTEM* /*system*/, FMOD_SYSTEM_CALLBACK_TYPE /*type*/, void *, void *, void *userData)
 {
-    mydsp_data_t *data = (mydsp_data_t *)dsp_state->plugindata;
-
-    /*
-        This loop assumes inchannels = outchannels, which it will be if the DSP is created with '0'
-        as the number of channels in FMOD_DSP_DESCRIPTION.
-        Specifying an actual channel count will mean you have to take care of any number of channels coming in,
-        but outputting the number of channels specified. Generally it is best to keep the channel
-        count at 0 for maximum compatibility.
-    */
-    for (unsigned int samp = 0; samp < length; samp++)
-    {
-        /*
-            Feel free to unroll this.
-        */
-        for (int chan = 0; chan < *outchannels; chan++)
-        {
-            /*
-                This DSP filter just halves the volume!
-                Input is modified, and sent to output.
-            */
-            data->buffer[(samp * *outchannels) + chan] = outbuffer[(samp * inchannels) + chan] = inbuffer[(samp * inchannels) + chan] * data->volume_linear;
-        }
-    }
-
-    data->channels = inchannels;
+    int *recordListChangedCount = (int *)userData;
+    *recordListChangedCount = *recordListChangedCount + 1;
 
     return FMOD_OK;
-}
-
-/*
-    Callback called when DSP is created.   This implementation creates a structure which is attached to the dsp state's 'plugindata' member.
-*/
-FMOD_RESULT F_CALLBACK myDSPCreateCallback(FMOD_DSP_STATE *dsp_state)
-{
-    unsigned int blocksize;
-    FMOD_RESULT result;
-
-    result = dsp_state->functions->getblocksize(dsp_state, &blocksize);
-    ERRCHECK(result);
-
-    mydsp_data_t *data = (mydsp_data_t *)calloc(sizeof(mydsp_data_t), 1);
-    if (!data)
-    {
-        return FMOD_ERR_MEMORY;
-    }
-    dsp_state->plugindata = data;
-    data->volume_linear = 1.0f;
-    data->length_samples = blocksize;
-
-    data->buffer = (float *)malloc(blocksize * 8 * sizeof(float));      // *8 = maximum size allowing room for 7.1.   Could ask dsp_state->functions->getspeakermode for the right speakermode to get real speaker count.
-    if (!data->buffer)
-    {
-        return FMOD_ERR_MEMORY;
-    }
-
-    return FMOD_OK;
-}
-
-/*
-    Callback called when DSP is destroyed.   The memory allocated in the create callback can be freed here.
-*/
-FMOD_RESULT F_CALLBACK myDSPReleaseCallback(FMOD_DSP_STATE *dsp_state)
-{
-    if (dsp_state->plugindata)
-    {
-        mydsp_data_t *data = (mydsp_data_t *)dsp_state->plugindata;
-
-        if (data->buffer)
-        {
-            free(data->buffer);
-        }
-
-        free(data);
-    }
-
-    return FMOD_OK;
-}
-
-/*
-    Callback called when DSP::getParameterData is called.   This returns a pointer to the raw floating point PCM data.
-    We have set up 'parameter 0' to be the data parameter, so it checks to make sure the passed in index is 0, and nothing else.
-*/
-FMOD_RESULT F_CALLBACK myDSPGetParameterDataCallback(FMOD_DSP_STATE *dsp_state, int index, void **data, unsigned int *length, char *)
-{
-    if (index == 0)
-    {
-        unsigned int blocksize;
-        FMOD_RESULT result;
-        mydsp_data_t *mydata = (mydsp_data_t *)dsp_state->plugindata;
-
-        result = dsp_state->functions->getblocksize(dsp_state, &blocksize);
-        ERRCHECK(result);
-
-        *data = (void *)mydata;
-        *length = blocksize * 2 * (int)sizeof(float);
-
-        return FMOD_OK;
-    }
-
-    return FMOD_ERR_INVALID_PARAM;
-}
-
-/*
-    Callback called when DSP::setParameterFloat is called.   This accepts a floating point 0 to 1 volume value, and stores it.
-    We have set up 'parameter 1' to be the volume parameter, so it checks to make sure the passed in index is 1, and nothing else.
-*/
-FMOD_RESULT F_CALLBACK myDSPSetParameterFloatCallback(FMOD_DSP_STATE *dsp_state, int index, float value)
-{
-    if (index == 1)
-    {
-        mydsp_data_t *mydata = (mydsp_data_t *)dsp_state->plugindata;
-
-        mydata->volume_linear = value;
-
-        return FMOD_OK;
-    }
-
-    return FMOD_ERR_INVALID_PARAM;
-}
-
-/*
-    Callback called when DSP::getParameterFloat is called.   This returns a floating point 0 to 1 volume value.
-    We have set up 'parameter 1' to be the volume parameter, so it checks to make sure the passed in index is 1, and nothing else.
-    An alternate way of displaying the data is provided, as a string, so the main app can use it.
-*/
-FMOD_RESULT F_CALLBACK myDSPGetParameterFloatCallback(FMOD_DSP_STATE *dsp_state, int index, float *value, char *valstr)
-{
-    if (index == 1)
-    {
-        mydsp_data_t *mydata = (mydsp_data_t *)dsp_state->plugindata;
-
-        *value = mydata->volume_linear;
-        if (valstr)
-        {
-            sprintf(valstr, "%d", (int)((*value * 100.0f)+0.5f));
-        }
-
-        return FMOD_OK;
-    }
-
-    return FMOD_ERR_INVALID_PARAM;
 }
 
 int FMOD_Main()
 {
-    FMOD::System       *system;
-    FMOD::Sound        *sound;
-    FMOD::Sound        *record;
-    FMOD::Channel      *channel;
+    RECORD_STATE record[MAX_DRIVERS] = { };
     FMOD::DSP          *mydsp;
     FMOD::ChannelGroup *mastergroup;
-    FMOD_RESULT         result;
-    unsigned int        version;
-    void               *extradriverdata = NULL;
-
-    #define LATENCY_MS      (50) /* Some devices will require higher latency to avoid glitches */
-    #define DRIFT_MS        (1)
-    #define DEVICE_INDEX    (0)
-    unsigned int samplesRecorded = 0;
-    unsigned int samplesPlayed = 0;
 
 
-    Common_Init(&extradriverdata);
+    void *extraDriverData = NULL;
+    Common_Init(&extraDriverData);
 
     /*
         Create a System object and initialize.
     */
-    result = FMOD::System_Create(&system);
+    FMOD::System *system = NULL;
+    FMOD_RESULT result = FMOD::System_Create(&system);
     ERRCHECK(result);
 
+    unsigned int version = 0;
     result = system->getVersion(&version);
     ERRCHECK(result);
 
@@ -202,62 +60,17 @@ int FMOD_Main()
         Common_Fatal("FMOD lib version %08x doesn't match header version %08x", version, FMOD_VERSION);
     }
 
-    result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+    result = system->init(100, FMOD_INIT_NORMAL, extraDriverData);
     ERRCHECK(result);
 
-    /* RECORD */
-    {
-      int numDrivers = 0;
-      result = system->getRecordNumDrivers(NULL, &numDrivers);
-      ERRCHECK(result);
-
-      if (numDrivers == 0)
-      {
-          Common_Fatal("No recording devices found/plugged in!  Aborting.");
-      }
-
-      /*
-          Determine latency in samples.
-      */
-      int nativeRate = 0;
-      int nativeChannels = 0;
-      result = system->getRecordDriverInfo(DEVICE_INDEX, NULL, 0, NULL, &nativeRate, NULL, &nativeChannels, NULL);
-      ERRCHECK(result);
-
-      unsigned int driftThreshold = (nativeRate * DRIFT_MS) / 1000;       /* The point where we start compensating for drift */
-      unsigned int desiredLatency = (nativeRate * LATENCY_MS) / 1000;     /* User specified latency */
-      unsigned int adjustedLatency = desiredLatency;                      /* User specified latency adjusted for driver update granularity */
-      int actualLatency = desiredLatency;                                 /* Latency measured once playback begins (smoothened for jitter) */
-
-      /*
-          Create user sound to record into, then start recording.
-      */
-      FMOD_CREATESOUNDEXINFO exinfo = {};
-      exinfo.cbsize           = sizeof(FMOD_CREATESOUNDEXINFO);
-      exinfo.numchannels      = nativeChannels;
-      exinfo.format           = FMOD_SOUND_FORMAT_PCM16;
-      exinfo.defaultfrequency = nativeRate;
-      exinfo.length           = nativeRate * (int)sizeof(short) * nativeChannels; /* 1 second buffer, size here doesn't change latency */
-
-      result = system->createSound(0, FMOD_LOOP_NORMAL | FMOD_OPENUSER, &exinfo, &record);
-      //result = system->createSound("media/piano.mp3", FMOD_LOOP_NORMAL, 0, &sound);
-      ERRCHECK(result);
-
-      result = system->recordStart(DEVICE_INDEX, record, true);
-      ERRCHECK(result);
-
-      unsigned int soundLength = 0;
-      result = record->getLength(&soundLength, FMOD_TIMEUNIT_PCM);
-      ERRCHECK(result);
-
-    } /* RECORD */
-
-
-    result = system->createSound("media/piano.mp3", FMOD_LOOP_NORMAL, 0, &sound);
+    /*
+        Setup a callback so we can be notified if the record list has changed.
+    */
+    int recordListChangedCount = 0;
+    result = system->setUserData(&recordListChangedCount);
     ERRCHECK(result);
 
-    result = system->playSound(sound, 0, false, &channel);
-    result = system->playSound(record, 0, false, &channel);
+    result = system->setCallback(&SystemCallback, FMOD_SYSTEM_CALLBACK_RECORDLISTCHANGED);
     ERRCHECK(result);
 
     /*
@@ -272,17 +85,104 @@ int FMOD_Main()
     result = mastergroup->addDSP(0, mydsp);
     ERRCHECK(result);
 
+
     /*
-        Main loop.
+        Main loop
     */
     do
     {
-
         Common_Update();
+
+        int numDrivers = 0;
+        int numConnected = 0;
+        result = system->getRecordNumDrivers(&numDrivers, &numConnected);
+        ERRCHECK(result);
+
+        numDrivers = Common_Min(numDrivers, MAX_DRIVERS); /* Clamp the reported number of drivers to simplify this example */
+
+        if (Common_BtnPress(BTN_ACTION1))
+        {
+            bool isRecording = false;
+            system->isRecording(0, &isRecording);
+
+            if (isRecording)
+            {
+                system->recordStop(0);
+            }
+            else
+            {
+                /* Clean up previous record sound */
+                if (record[0].sound)
+                {
+                    result = record[0].sound->release();
+                    ERRCHECK(result);
+                }
+
+                /* Query device native settings and start a recording */
+                int nativeRate = 0;
+                int nativeChannels = 0;
+                result = system->getRecordDriverInfo(0, NULL, 0, NULL, &nativeRate, NULL, &nativeChannels, NULL);
+                ERRCHECK(result);
+
+                FMOD_CREATESOUNDEXINFO exinfo = {};
+                exinfo.cbsize           = sizeof(FMOD_CREATESOUNDEXINFO);
+                exinfo.numchannels      = nativeChannels;
+                exinfo.format           = FMOD_SOUND_FORMAT_PCM16;
+                exinfo.defaultfrequency = nativeRate;
+                exinfo.length           = nativeRate * (int) sizeof(short) * nativeChannels; /* 1 second buffer, size here doesn't change latency */
+
+                result = system->createSound(0, FMOD_LOOP_NORMAL | FMOD_OPENUSER, &exinfo, &record[0].sound);
+                ERRCHECK(result);
+
+                result = system->recordStart(0, record[0].sound, true);
+                if (result != FMOD_ERR_RECORD_DISCONNECTED)
+                {
+                    ERRCHECK(result);
+                }
+            }
+            Common_Sleep(50);
+
+            bool isPlaying = false;
+            record[0].channel->isPlaying(&isPlaying);
+
+            if (isPlaying)
+            {
+                record[0].channel->stop();
+            }
+            else if (record[0].sound)
+            {
+                result = system->playSound(record[0].sound, NULL, false, &record[0].channel);
+                ERRCHECK(result);
+            }
+        }
 
 
         result = system->update();
         ERRCHECK(result);
+
+        Common_Draw("Record list has updated %d time(s).", recordListChangedCount);
+        Common_Draw("Currently %d device(s) plugged in.", numConnected);
+        Common_Draw("");
+        Common_Draw("Press %s to quit", Common_BtnStr(BTN_QUIT));
+        Common_Draw("Press %s start / stop recording", Common_BtnStr(BTN_ACTION1));
+        Common_Draw("Press %s start / stop playback", Common_BtnStr(BTN_ACTION2));
+        Common_Draw("");
+        int numDisplay = Common_Min(numDrivers, MAX_DRIVERS_IN_VIEW);
+
+        char name[256];
+        int sampleRate;
+        int channels;
+        FMOD_GUID guid;
+        FMOD_DRIVER_STATE state;
+
+        result = system->getRecordDriverInfo(0, name, sizeof(name), &guid, &sampleRate, NULL, &channels, &state);
+        ERRCHECK(result);
+
+        bool isRecording = false;
+        system->isRecording(0, &isRecording);
+
+        bool isPlaying = false;
+        record[0].channel->isPlaying(&isPlaying);
 
         {
             FMOD_DSP_PARAMETER_DESC *desc;
@@ -291,14 +191,7 @@ int FMOD_Main()
             result = mydsp->getParameterData(FMOD_DSP_FFT_SPECTRUMDATA, (void **)&data, 0, 0, 0);
             ERRCHECK(result);
 
-            /*Common_Draw("==================================================");
-            Common_Draw("QYUBEE");
-            Common_Draw("==================================================");
-            Common_Draw("");
-            Common_Draw("Press %s to quit", Common_BtnStr(BTN_QUIT));
-            Common_Draw("");
-            Common_Draw("Test : %s", desc->name);
-            */
+            /* AFFICHE FFT*/
             const int a=(const int)data->length/10;
             const int b=60;
             std::vector<std::vector<char> > v( a , std::vector<char> (b, 0));
@@ -314,6 +207,7 @@ int FMOD_Main()
                 }
               }
             }
+
             for (int i = b-1; i > 0; i--) {
               for (int bin = 0; bin < a; bin++)  {
                 std::cout << v[bin][i];
@@ -332,22 +226,19 @@ int FMOD_Main()
             Common_Draw(desc->name);
             Common_Draw(d);
           }
+
         Common_Sleep(50);
     } while (!Common_BtnPress(BTN_QUIT));
 
-    /*
-        Shut down
-    */
-    result = sound->release();
-    ERRCHECK(result);
+    for (int i = 0; i < MAX_DRIVERS; i++)
+    {
+        if (record[i].sound)
+        {
+            result = record[i].sound->release();
+            ERRCHECK(result);
+        }
+    }
 
-    result = mastergroup->removeDSP(mydsp);
-    ERRCHECK(result);
-    result = mydsp->release();
-    ERRCHECK(result);
-
-    result = system->close();
-    ERRCHECK(result);
     result = system->release();
     ERRCHECK(result);
 
